8efd524faec4fff1dab0cb76a7797e07daa6091d
Taynara <tay.dutra@hotmail.com>
2021-05-20 11:19:38 -0700

    Implementado método buscar

--- arvore_publica.h
+++ arvore_publica.h
@@ -10,7 +10,7 @@ int destroiABB(pABB pp);
 int reiniciaABB(pABB p);
 int insereABB(pABB p, void *novo, int (* cmp)(void *p1, void *p2)); // ok
 int removeABB(pABB p, void *item, int (* cmp)(void *p1, void *p2));
-int buscaABB(pABB p, void *item, int (* cmp)(void *p1, void *p2), int (* cmpI)(void *p1, void *p2));
+int buscaABB(pABB p, void *item, int (* cmp)(void *p1, void *p2), int (* cmpI)(void *p1, void *p2)); //ok 
 int percursoEmOrdem(pABB pa, void (* processa)(void *p)); // ok
 int percursoPreOrdem(pABB pa, void (* processa)(void *p)); // ok
 int percursoPosOrdem(pABB pa, void (* processa)(void *p)); // ok
